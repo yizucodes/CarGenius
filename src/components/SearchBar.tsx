36adb19cef7c122b2d2ef29e2c32fa5d5f024f78
Yi Zu <yizucodes@gmail.com>
2023-12-29 10:55:47 -0500

    Add SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='Search by car model'
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
