bddcc8e20e1dfc35ad0403b6c5db755b0c20a07d
Yi Zu <yizucodes@gmail.com>
2023-12-29 10:56:09 -0500

    Show SearchBar and CarList

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import CarList from './components/CarList';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [searchResults, setSearchResults] = useState([
+    { make: 'Toyota', model: 'Camry', year: 2020, price: 25000 },
+    { make: 'Honda', model: 'Accord', year: 2019, price: 22000 },
+  ]);
+
+  const handleSearch = (query: string) => {
+    const results = searchResults.filter((car) =>
+      car.model.toLowerCase().includes(query.toLowerCase())
+    );
+
+    setSearchResults(results);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Car Model Search</h1>
+      <SearchBar onSearch={handleSearch} />
+      <CarList cars={searchResults} />
+    </div>
+  );
+  // End of component
+};
 
-export default App
+export default App;
