8a513785844ecb19f4816cea2de307baa9c70986
Yi Zu <yizucodes@gmail.com>
2023-12-29 11:16:09 -0500

    Global var for cars, filter from this global var

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
 import CarList from './components/CarList';
 
+const ALL_CARS = [
+  { make: 'Toyota', model: 'Camry', year: 2020, price: 25000 },
+  { make: 'Honda', model: 'Accord', year: 2019, price: 22000 },
+];
+
 const App: React.FC = () => {
-  const [searchResults, setSearchResults] = useState([
-    { make: 'Toyota', model: 'Camry', year: 2020, price: 25000 },
-    { make: 'Honda', model: 'Accord', year: 2019, price: 22000 },
-  ]);
+  const [searchResults, setSearchResults] = useState(ALL_CARS);
 
   const handleSearch = (query: string) => {
-    const results = searchResults.filter((car) =>
+    const results = ALL_CARS.filter((car) =>
       car.model.toLowerCase().includes(query.toLowerCase())
     );
 
